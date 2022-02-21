6ed594aadd8027921e9331593d2b2cad9b3f7409
Marisa de la Calle Brihuega <marisa.dlcb@gmail.com>
2022-02-21 15:31:21 +0100

    Template html y estilos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { dashboardRoutes } from './dashboard/dashboard.routes';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: '',
+    component: DashboardComponent,
+    children: dashboardRoutes
+  },
+  { path: '**', redirectTo: '' } // Cualquier otro path me llevará al dashboard
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
